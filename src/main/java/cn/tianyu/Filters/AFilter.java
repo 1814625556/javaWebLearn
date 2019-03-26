b83e075be92a5ba246ff329474ca73383bcc66ee
chenchang <chenchang@kezaihui.com>
2019-03-26 19:58:29 +0800

    filter

--- src/main/java/cn/tianyu/Filters/AFilter.java
+++ src/main/java/cn/tianyu/Filters/AFilter.java
@@ -1,14 +1,23 @@
-@javax.servlet.annotation.WebFilter(filterName = "AFilter")
-public class AFilter implements javax.servlet.Filter {
+package cn.tianyu.Filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+public class AFilter implements Filter {
     public void destroy() {
+        System.out.println("AFilter destroy....");
     }
 
-    public void doFilter(javax.servlet.ServletRequest req, javax.servlet.ServletResponse resp, javax.servlet.FilterChain chain) throws javax.servlet.ServletException, java.io.IOException {
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws ServletException, IOException {
+        System.out.println("AFilter doFilter....");
         chain.doFilter(req, resp);
+        System.out.println("AFilter doFilter after....");
     }
 
-    public void init(javax.servlet.FilterConfig config) throws javax.servlet.ServletException {
-
+    public void init(FilterConfig config) throws ServletException {
+        System.out.println("AFilter init....");
     }
 
 }
