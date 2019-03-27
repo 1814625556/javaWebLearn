fd480a83b87892a51a09c4bc0fc7431cbbdae7ab
chenchang <chenchang@kezaihui.com>
2019-03-27 17:09:30 +0800

    过滤器

--- src/main/java/cn/tianyu/Filters/AFilter.java
+++ src/main/java/cn/tianyu/Filters/AFilter.java
@@ -3,6 +3,7 @@ package cn.tianyu.Filters;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
+import java.util.Enumeration;
 
 public class AFilter implements Filter {
     public void destroy() {
@@ -16,8 +17,22 @@ public class AFilter implements Filter {
         System.out.println("AFilter doFilter after....");
     }
 
+    /*
+    * 探究FilterConfig
+    * */
     public void init(FilterConfig config) throws ServletException {
         System.out.println("AFilter init....");
+        String filterName = config.getFilterName();
+        String val = config.getInitParameter("firstName");
+        Enumeration names = config.getInitParameterNames();
+        System.out.println(filterName);
+        System.out.println(val);
+        while(names.hasMoreElements())
+        {
+            String name =  (String) names.nextElement();
+            String value = config.getInitParameter(name);
+            System.out.println(name+" = "+value);
+        }
     }
 
 }
